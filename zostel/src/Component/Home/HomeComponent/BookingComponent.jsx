313cca8c529f64cf1c052c9ca186322dfb7c73ed
DenishFuletra <fuletradenish@gmail.com>
2023-01-08 18:31:21 +0530

    commit

--- zostel/src/Component/Home/HomeComponent/BookingComponent.jsx
+++ zostel/src/Component/Home/HomeComponent/BookingComponent.jsx
@@ -39,67 +39,70 @@ export default function BookingComponent() {
 
     }
     return (
-        <div id="booking-container">
-            <div id="booking-place">
-                <h3 style={option==="DESTINATION"?{borderBottom:"2px solid red"}:{borderBottom:"none"}} onClick={() => setOption("DESTINATION")} className="booking-place-hover">DESTINATION</h3>
-                <h3 style={option==="ZOSTEL"?{borderBottom:"2px solid red"}:{borderBottom:"none"}} onClick={() => setOption("ZOSTEL")} className="booking-place-hover">ZOSTEL</h3>
-                <h3 style={option==="ZOSTEL HOMES"?{borderBottom:"2px solid red"}:{borderBottom:"none"}} onClick={() => setOption("ZOSTEL HOMES")} className="booking-place-hover">ZOSTEL HOMES</h3>
-                <h3 style={option==="ZOSTEL PLUS"?{borderBottom:"2px solid red"}:{borderBottom:"none"}} onClick={() => setOption("ZOSTEL PLUS")} className="booking-place-hover">ZOSTEL PLUS</h3>
-            </div>
-            <div id="booking-time">
-                <div id="booking-place-div">
-                    <h5>SELECT YOUR DESTINATION</h5>
-                    <div id="booking-select">
-                        <Select
-                            defaultValue={selectedOption}
-                            onChange={setSelectedOption}
-                            options={options}
-                            placeholder={"eg. Manali, Jothpur, Jaipur, etc."}
-                        />
-                    </div>
+        <div id="main-booking-container">
+            <h1 style={{ textAlign: "center", color: "white", fontWeight: "bolder", fontSize: "50px" }}>Live it. Now</h1>
+            <div id="booking-container">
+                <div id="booking-place">
+                    <h3 style={option === "DESTINATION" ? { borderBottom: "2px solid red" } : { borderBottom: "none" }} onClick={() => setOption("DESTINATION")} className="booking-place-hover">DESTINATION</h3>
+                    <h3 style={option === "ZOSTEL" ? { borderBottom: "2px solid red" } : { borderBottom: "none" }} onClick={() => setOption("ZOSTEL")} className="booking-place-hover">ZOSTEL</h3>
+                    <h3 style={option === "ZOSTEL HOMES" ? { borderBottom: "2px solid red" } : { borderBottom: "none" }} onClick={() => setOption("ZOSTEL HOMES")} className="booking-place-hover">ZOSTEL HOMES</h3>
+                    <h3 style={option === "ZOSTEL PLUS" ? { borderBottom: "2px solid red" } : { borderBottom: "none" }} onClick={() => setOption("ZOSTEL PLUS")} className="booking-place-hover">ZOSTEL PLUS</h3>
                 </div>
-                <div id="booking-checkin-out">
-                    <div className="booking-checkin">
-                        <h5>CHECK IN</h5>
-                        <div >
-                            <DatePicker
-                                selected={startDate}
-                                onChange={(date) => setStartDate(date)}
-                                selectsStart
-                                monthsShown={2}
-                                startDate={startDate}
-                                dateFormat="dd/MM/yyyy"
-                                endDate={endDate}
-                                minDate={new Date()}
-                            // excludeDates={[new Date(), subDays(new Date(), 1)]}
+                <div id="booking-time">
+                    <div id="booking-place-div">
+                        <h5>SELECT YOUR DESTINATION</h5>
+                        <div id="booking-select">
+                            <Select
+                                defaultValue={selectedOption}
+                                onChange={setSelectedOption}
+                                options={options}
+                                placeholder={"eg. Manali, Jothpur, Jaipur, etc."}
                             />
                         </div>
                     </div>
-                    <div>
-                        <i class="fa-solid fa-right-long"></i>
-                    </div>
-                    <div className="booking-checkin">
-                        <h5>CHECK OUT</h5>
+                    <div id="booking-checkin-out">
+                        <div className="booking-checkin">
+                            <h5>CHECK IN</h5>
+                            <div >
+                                <DatePicker
+                                    selected={startDate}
+                                    onChange={(date) => setStartDate(date)}
+                                    selectsStart
+                                    monthsShown={2}
+                                    startDate={startDate}
+                                    dateFormat="dd/MM/yyyy"
+                                    endDate={endDate}
+                                    minDate={new Date()}
+                                // excludeDates={[new Date(), subDays(new Date(), 1)]}
+                                />
+                            </div>
+                        </div>
                         <div>
-                            <DatePicker
-                                selected={endDate}
-                                onChange={(date) => setEndDate(date)}
-                                selectsEnd
-                                startDate={startDate}
-                                endDate={endDate}
-                                minDate={startDate}
-                                monthsShown={2}
-                                closeOnScroll={true}
-                                dateFormat="dd/MM/yyyy"
-                            />
+                            <i class="fa-solid fa-right-long"></i>
+                        </div>
+                        <div className="booking-checkin">
+                            <h5>CHECK OUT</h5>
+                            <div>
+                                <DatePicker
+                                    selected={endDate}
+                                    onChange={(date) => setEndDate(date)}
+                                    selectsEnd
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    minDate={startDate}
+                                    monthsShown={2}
+                                    closeOnScroll={true}
+                                    dateFormat="dd/MM/yyyy"
+                                />
+                            </div>
                         </div>
                     </div>
+                    <div id="booking-button">
+                        <button onClick={HandleSubmit}>Book Now</button>
+                    </div>
                 </div>
-                <div id="booking-button">
-                    <button onClick={HandleSubmit}>Book Now</button>
-                </div>
-            </div>
 
+            </div>
         </div>
 
     )
